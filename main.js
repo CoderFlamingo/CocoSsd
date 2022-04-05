0b34476389d7ea93711e006927d39d52fe3212ce
CoderFlamingo <101107754+CoderFlamingo@users.noreply.github.com>
2022-04-05 18:50:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+objectDetector= "";
+
 img = "";
 status = "";
+objects = [];
 
 function setup() {
     canvas = createCanvas(640, 420);
@@ -12,10 +15,22 @@ function preload() {
 }
 function draw() {
     image(img, 0, 0, 640, 420);
+    if (status != "") { // printing the message - detected - to the user
+        for (var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+      
+            fill(255, 0, 0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(255, 0, 0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+          }
+    }
     fill("#ff0000");
     text("Dog", 45, 75);
     noFill();
-    stroke("#000000");
+    stroke("#ff0000");
     rect(30, 60, 450, 350);
 
     fill("#ff0000");
@@ -35,4 +50,5 @@ function gotResult(error, results) {
         console.log(error);
     }
     console.log(results);
+    object = results;
 }
