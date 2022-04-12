25f54acd3b0762a83e421b4d87fb47127a55af7a
CoderFlamingo <101107754+CoderFlamingo@users.noreply.github.com>
2022-04-12 18:43:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,45 +5,41 @@ status = "";
 objects = [];
 
 function setup() {
-    canvas = createCanvas(640, 420);
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(380, 380);
+    canvas = createCanvas(380, 420);
     canvas.center();
-    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 function preload() {
     img = loadImage("dog_cat.jpg");
 }
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
     if (status != "") { // printing the message - detected - to the user
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
         for (var i = 0; i < objects.length; i++) {
             document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "No. of object detected are: " + objects.length;
       
-            fill(255, 0, 0);
+            fill(r,g,b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
-            stroke(255, 0, 0);
+            stroke(r,g,b);
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
           }
     }
-    fill("#ff0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#ff0000");
-    rect(30, 60, 450, 350);
-
-    fill("#ff0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#ff0000");
-    rect(300, 90, 270, 320);
 }
 
 function modelLoaded() {
     console.log("Model Loaded!")
     status = true;
-    objectDetector.detect(img, gotResult);
 }
 function gotResult(error, results) {
     if (error) {
